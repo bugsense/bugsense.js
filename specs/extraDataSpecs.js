84ff0e21091e17a44091b3c692c6f914b786c976
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2013-10-29 10:28:41 +0200

    Adds specs for removing extra data

--- specs/extraDataSpecs.js
+++ specs/extraDataSpecs.js
@@ -1,7 +1,21 @@
 describe("Bugsense::ExtraData", function(){
-  it("should add extra data correctly", function(){
+  it("should add some initial extra data", function(){
     bugsense.addExtraData( 'user_type', 'CEO' );
-    expect(bugsense.extraData.length).not.toEqual(0);
+    expect(Object.keys(bugsense.extraData).length).toEqual(1);
     expect(bugsense.extraData.user_type).toBe('CEO');
+  });
+  it("should add other extra data as well", function(){
+    bugsense.addExtraData( 'paying', true );
+    expect(Object.keys(bugsense.extraData).length).toEqual(2);
+    expect(bugsense.extraData.user_type).toBe('CEO');
+    expect(bugsense.extraData.paying).toBeTruthy();
+  });
+  it("should remove the initial extra data added", function(){
+    bugsense.removeExtraData( 'user_type' );
+    expect(Object.keys(bugsense.extraData).length).toEqual(1);
+    expect(bugsense.extraData.user_type).toBeUndefined();
+    bugsense.removeExtraData( 'paying' );
+    expect(Object.keys(bugsense.extraData).length).toEqual(0);
+    expect(bugsense.extraData.paying).toBeUndefined();
   });
 });
