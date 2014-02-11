92bd99e46e2fc26f31b1607dd602d5920d7ff56c
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-02-11 15:45:06 +0200

    First implementation for channel, extraData and breadcrumbs

--- specs/breadcrumbsSpecs.js
+++ specs/breadcrumbsSpecs.js
@@ -1,24 +1,24 @@
 describe("Bugsense::Breadcrumbs", function(){
   it("should add some initial breadcrumb", function(){
-    bugsense.leaveBreadcrumb( 'button_clicked' );
-    expect(bugsense.breadcrumbs.length).toEqual(1);
-    expect(bugsense.breadcrumbs[0]).toBe('button_clicked');
+    Bugsense.leaveBreadcrumb( 'button_clicked' );
+    expect(Bugsense.breadcrumbs.length).toEqual(1);
+    expect(Bugsense.breadcrumbs[0]).toBe('button_clicked');
   });
   it("should add other breadcrumb as well", function(){
-    bugsense.leaveBreadcrumb( 'added_credit_card' );
-    expect(bugsense.breadcrumbs.length).toEqual(2);
-    expect(bugsense.breadcrumbs[1]).toBe('added_credit_card');
+    Bugsense.leaveBreadcrumb( 'added_credit_card' );
+    expect(Bugsense.breadcrumbs.length).toEqual(2);
+    expect(Bugsense.breadcrumbs[1]).toBe('added_credit_card');
   });
   it("should clear all breadcrumbs", function(){
-    expect(bugsense.breadcrumbs.length).toEqual(2);
-    bugsense.clearBreadcrumbs();
-    expect(bugsense.breadcrumbs.length).toBe(0);
-    expect(bugsense.breadcrumbs[0]).toBeUndefined();
-    expect(bugsense.breadcrumbs[1]).toBeUndefined();
+    expect(Bugsense.breadcrumbs.length).toEqual(2);
+    Bugsense.clearBreadcrumbs();
+    expect(Bugsense.breadcrumbs.length).toBe(0);
+    expect(Bugsense.breadcrumbs[0]).toBeUndefined();
+    expect(Bugsense.breadcrumbs[1]).toBeUndefined();
   });
   it("should add other breadcrumb after clearing all breadcrumbs", function(){
-    bugsense.leaveBreadcrumb( 'after_clearing' );
-    expect(bugsense.breadcrumbs.length).toEqual(1);
-    expect(bugsense.breadcrumbs[0]).toBe('after_clearing');
+    Bugsense.leaveBreadcrumb( 'after_clearing' );
+    expect(Bugsense.breadcrumbs.length).toEqual(1);
+    expect(Bugsense.breadcrumbs[0]).toBe('after_clearing');
   });
 });
