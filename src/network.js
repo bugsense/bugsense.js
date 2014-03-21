f5b0966b5d58bac7d3378626d682e44e77f96273
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-03-21 17:51:32 +0200

    Support for dynamic callback on network calls, using closures

--- src/network.js
+++ src/network.js
@@ -8,23 +8,7 @@ Bugsense.Network = (function() {
       return "https://ticks.bugsense.com/"+Bugsense.get('apiKey')+"/"+Bugsense.get('uid');
     },
     sendCrash: function(data) {
-      this.send(param({ data: JSON.stringify(data) }), 'POST', this.getCrashURL())
-    },
-    sendEvent: function(data) {
-      this.send(data, 'POST', this.getTicksURL())
-    },
-    send: function(data, method, url) {
-      // Send the data over to Bugsense
-      var net = new XMLHttpRequest();
-      net.open(method, url, true );
-      net.setRequestHeader('X-BugSense-Api-Key', Bugsense.config.apiKey);
-      net.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      var that = this;
-      net.onerror = function (a) {
-        /* cache the report */
-        Bugsense.Cache.save(data);
-      }
-      function successHandler() {
+      this.send(param({ data: JSON.stringify(data) }), 'POST', this.getCrashURL(), function(net) {
         if (net && net.readyState != 4) { return; }
         if (net && net.status != 200) {
           return false;
@@ -33,9 +17,26 @@ Bugsense.Network = (function() {
         if ('console' in window) {
           console.log('logged 1 error to Bugsense, status: ' + net.responseText);
         }
-      };
+      })
+    },
+    sendEvent: function(data) {
+      this.send(data, 'POST', this.getTicksURL())
+    },
+    send: function(data, method, url, successHandler) {
+      // Send the data over to Bugsense
+      var net = new XMLHttpRequest();
+      net.open(method, url, true );
+      net.setRequestHeader('X-BugSense-Api-Key', Bugsense.config.apiKey);
+      net.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      var that = this;
+      net.onerror = function (a) {
+        /* cache data */
+        Bugsense.Cache.save(data);
+      }
 
-      net.onreadystatechange = successHandler;
+      if(successHandler)
+        net.onreadystatechange = (successHandler)(net);
+
       net.send(data);
     }
   };
