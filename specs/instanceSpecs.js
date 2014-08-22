8deec050e9bddc01f4159eb818b93a6901c9690e
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-08-22 13:08:08 -0700

    Run specs before release to build everything

--- specs/instanceSpecs.js
+++ specs/instanceSpecs.js
@@ -66,9 +66,9 @@ describe('Bugsense::Unique ID', function() {
 });
 xdescribe('Bugsense::Data fixture', function () {
   it("should have correct data fixture", function(){
-    expect(bugsense.config.VERSION).toBe('2.0.1');
+    expect(bugsense.config.VERSION).toBe('2.1.0');
     expect(bugsense.dataFixture.client.name).toBe("bugsense-js");
-    expect(bugsense.dataFixture.client.version).toBe("2.0.1");
+    expect(bugsense.dataFixture.client.version).toBe("2.1.0");
     expect(bugsense.dataFixture.application_environment.appname).toBe("theApp");
     expect(bugsense.dataFixture.application_environment.appver).toBe("1.1.1");
     expect(bugsense.dataFixture.application_environment.osver).toMatch(/Intel Mac OS X|Linux x86_64/);
