a82abd397eaab91d0ebfb4c3a374f393ee432577
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-11-13 23:17:24 +0200

    Fixes grunt rig task for AMD builds, naming bugsense module to avoid requirejs errors

--- src/amd.js
+++ src/amd.js
@@ -1,7 +1,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(factory);
+        define('bugsense', [], factory);
     } else {
         // Browser globals
         root.Bugsense = factory(root.b);
